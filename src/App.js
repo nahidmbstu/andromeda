f8cb0fdca2b3db44e565972d8820a64a77edd404
nahid <nahidmbstu>
2020-02-09 17:27:22 +0600

    added image

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { createMuiTheme, makeStyles } from "@material-ui/core/styles";
-import Toolbar from "@material-ui/core/Toolbar";
-import { palette, spacing, typography } from "@material-ui/system";
 import React from "react";
-import styled from "styled-components";
 import "./App.css";
 import PrimaryAppBar from "./Components/Appbar";
-
-const Box = styled.div`${palette}${spacing}${typography}`;
-// or import Box from '@material-ui/core/Box';
+import MainBanner from "./Components/MainBanner";
 
 const theme = createMuiTheme();
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
     flexGrow: 1
   }
 }));
@@ -32,12 +20,9 @@ function App(props) {
     <React.Fragment>
       <CssBaseline />
       <PrimaryAppBar />
-      <Toolbar />
-      <Container>
-        <Box color='primary.main' bgcolor='background.paper' fontFamily='h6.fontFamily' fontSize={{ xs: "h6.fontSize", sm: "h4.fontSize", md: "h3.fontSize" }} p={{ xs: 2, sm: 3, md: 4 }}>
-          @material-ui/system
-        </Box>
-      </Container>
+      <div className={classes.root}>
+        <MainBanner />
+      </div>
     </React.Fragment>
   );
 }
--- src/Components/MainBanner.js
+++ src/Components/MainBanner.js
@@ -1,12 +1,15 @@
-import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import React from "react";
+import { ReactComponent as Logo } from "../app_landing.svg";
 
 const useStyles = makeStyles(theme => ({
   Main: {
-    height: "100vh",
-    width: window.innerWidth,
-    background: "rgba(0,0,0,0.3)"
+    width: "100vw",
+    background: "rgba(255,255,255,0.7)",
+    margin: 0,
+    padding: 0
   }
 }));
 
@@ -14,8 +17,15 @@ export default function MainBanner(props) {
   const classes = useStyles();
 
   return (
-    <Box height='100%' bgcolor='grey.300' mx={0.5} width={120} display='inline-block'>
-      Height 100%
-    </Box>
+    <div className={classes.Main}>
+      <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={6}>
+          <Typography variant='h6'>Andromeda</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Logo width={300} height={300} />
+        </Grid>
+      </Grid>
+    </div>
   );
 }
